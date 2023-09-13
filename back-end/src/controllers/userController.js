ac3d580964858163947fe08ee9983b62911b91a3
Daniel Saraiva <danieldsvn@gmail.com>
2023-09-13 11:56:29 -0300

    Instala dependencia mysql2 para conexao com banco de dados, configura dotenv, cria userService.js e userModel.js

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,16 +1,16 @@
 /* eslint-disable require-jsdoc */
+import createService from '../services/userService.js';
 
-
-export default async function create(req, res) {
+export default async function createController(req, res) {
   console.log('UserCreate controller');
-  // const data = req.body;
-  // const sales = await create(data);
-  // if (sales.message) {
-  //   const {code, message} = sales;
-  //   return res.status(code).json({message});
-  // }
+  const data = req.body;
+  const user = await createService(data);
+  if (user.message) {
+    const {code, message} = sales;
+    return res.status(code).json({message});
+  }
 
-  return res.status(201).json({message: 'testando'});
+  return user;
 };
 
 
---
+++ back-end/src/services/userService.js
@@ -0,0 +1,7 @@
+/* eslint-disable require-jsdoc */
+export default async function createService(data) {
+  console.log('UserCreate service');
+  const payLoad = await createModel(data);
+
+  return payLoad;
+};
