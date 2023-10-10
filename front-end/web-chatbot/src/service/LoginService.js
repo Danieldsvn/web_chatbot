95cc91f646b1592899542e040e837f9611d0993d
Daniel <danieldsvn@gmail.com>
2023-10-10 19:17:09 -0300

    Refatorando função fetchLogin

--- front-end/web-chatbot/src/service/LoginService.js
+++ front-end/web-chatbot/src/service/LoginService.js
@@ -6,22 +6,12 @@ export const fetchLogin = async (name, password) => {
       name: name,
       password: password,
     }),
-  };
-
-  const notFound = 404;
-  const statusOk = 200;
-  const badRequest = 400;
+  };  
 
   try {
     const response = await fetch('http://localhost:3001/login', options);
-    if (response.status === statusOk) {
-      const data = await response.json();
-      return data;
-    }
-    if (response.status === notFound || response.status == badRequest) {      
-      const data = await response.json();
-      return data;
-    }
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error(error);
   }
